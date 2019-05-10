653f187aed0ed1473781c8bb25a2c7688adfe9aa
Jaxon Porter <jaxonutah@gmail.com>
2019-05-10 12:07:59 -0600

    tested control flow

---
+++ hello.go
@@ -0,0 +1,26 @@
+// you will always start with package main
+// and exit with func main
+
+package main
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Everyone!\n")
+	foo()
+	fmt.Println("something more")
+
+	for i := 1; i <= 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+			foo()
+		}
+	}
+}
+
+func foo() {
+	fmt.Println("Im in foo\n")
+}
+// control flow:
+// (1) sequence
+// (2) loop: iterative
+// (3) conditional
