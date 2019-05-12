afcf45c3df1c24c9525fa94f5f805d1854beb57d
Jaxon Porter <jaxonutah@gmail.com>
2019-05-11 21:34:25 -0600

    worked with assigning values

--- operators.go
+++ operators.go
@@ -1,14 +1,28 @@
 package main
 import "fmt"
 
+// v := 'hi'
+
+
+
 func main() {
 	// := declares and assigns a value to variable
 	// you can reassign value with just an = sign later
-	x := 42
-	fmt.Println(x)
-	x = 99
-	fmt.Println(x)
-	y := 100 < 24
-	fmt.Println(y)
 
+
+	y := []string{"1", "2", "3"}
+	fmt.Println(y)
+	// fmt.Println(v)
+	a, b, c, d := f2()
+	fmt.Println(d)
+	fmt.Println(c)
+	fmt.Println(b)
+	fmt.Println(a)
 }
+
+func f2() (int, int, int, int) {
+	return 3, 4, 5, 6 
+}
+// func foo(x int) {
+// 	fmt.Println(x)
+// }
