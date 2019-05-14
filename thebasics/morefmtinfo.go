26b176c9719738c9b8ce0e5667f654788d4d057a
Jaxon Porter <jaxonutah@gmail.com>
2019-05-14 15:28:43 -0600

    fmt options

---
+++ thebasics/morefmtinfo.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var y = 987698
+
+func main() {
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+	fmt.Printf("%b\n", y)
+	fmt.Printf("%x\n", y)
+	fmt.Printf("%#x\t%T\t%b\t%v\t%#q\n", y, y, y, y, y)
+
+}
