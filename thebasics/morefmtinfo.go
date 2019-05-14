1b3118c15510c599deb02bbcfec6194f8a998410
Jaxon Porter <jaxonutah@gmail.com>
2019-05-14 15:33:50 -0600

    learned Sprint from fmt package

--- thebasics/morefmtinfo.go
+++ thebasics/morefmtinfo.go
@@ -10,5 +10,6 @@ func main() {
 	fmt.Printf("%b\n", y)
 	fmt.Printf("%x\n", y)
 	fmt.Printf("%#x\t%T\t%b\t%v\t%#q\n", y, y, y, y, y)
-
+	s := fmt.Sprintf("%#x\t%T\t%b\t%v\t%#q\n", y, y, y, y, y)
+	fmt.Println(s)
 }
