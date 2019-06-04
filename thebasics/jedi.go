f15269d51608f0f69a75d09c58311bd73eff43bb
Jaxon Porter <jaxonutah@gmail.com>
2019-06-04 14:14:59 -0600

    worked on go excersize

---
+++ thebasics/jedi.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Printf("%T\n", x)
+	fmt.Printf("%T\n", y)
+	fmt.Printf("%T\n", z)
+
+	// fmt.Println(x,y,z)
+}
